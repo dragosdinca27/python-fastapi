e57afa01406040272d52a187d9e986b2a2d150ca
Dragos Dinca <dragosdinca@Dragoss-MacBook-Air.local>
2023-05-17 13:26:23 +0300

    add tables and password

--- jobboard/backend/main.py
+++ jobboard/backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from core.config import settings
 from db.session import engine
-from db.base_class import Base
+from db.base import Base
 
 def create_tables():
-    Base.metadata.create_all(bind=engine)
+	print("create_tables")
+	Base.metadata.create_all(bind=engine)
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
